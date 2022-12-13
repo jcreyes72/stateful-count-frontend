ab54dd32f3f39947a4413f4fdc82209763af5a82
Julio Reyes <jcreyes@g.clemson.edu>
2022-12-13 11:06:55 -0500

    Implement separate increment/decrement functions

--- client/src/components/counter/Counter.jsx
+++ client/src/components/counter/Counter.jsx
@@ -3,11 +3,16 @@ import './Counter.css'
 
 const Counter = () => {
 
-  state = {
-    number: '',
-  };
 
   const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count+1);
+  }
+
+  const decrement = () => {
+    setCount(count-1);
+  }
 
   return (
   <div>
@@ -16,13 +21,13 @@ const Counter = () => {
           <h1 className="title">A Stateful Counter</h1>
         </div>
         <div className="grid-item grid-item-1">
-            <button className="button" onClick={() => setCount(count + 1)}>Increment</button>
+            <button className="button" onClick={increment}>Increment</button>
         </div>
         <div className="grid-item grid-item-2">
-            <span className="number">{this.state.number}</span>
+            <span className="number">{count}</span>
         </div>
         <div className="grid-item grid-item-3">
-            <button className="button" onClick={() => setCount(count - 1)}>Decrement</button>
+            <button className="button" onClick={decrement}>Decrement</button>
         </div>
     </div>
   </div>
